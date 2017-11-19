c1a6135d038dac1a4e771d7736cc714f0efd04b5
zhfkt <zhfkt@hotmail.com>
2017-11-19 22:27:49 +0800

    init first commit

---
+++ splitFrames.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if (( $# < 1 ))
+then
+	echo "splitFrames.sh [folder]";
+	exit 0;
+	
+else 
+
+	folder=$1
+	
+fi
+
+
+for i in `ls $folder`; 
+do 
+	echo ${i}; 
+	j=`cut -d '.' -f 1 <<< $i`; 
+	mkdir -p screenshot/${j}/; 
+	ffmpeg -i ${folder}/${i} screenshot/${j}/${j}_%d.png ; 
+done
