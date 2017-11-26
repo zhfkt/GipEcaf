33694eee5caf90257ace162fccae8148bdeaf788
zhfkt <zhfkt@hotmail.com>
2017-11-26 17:17:07 +0800

    Improve segmentation

--- segmentationForTrainAndTest.sh
+++ segmentationForTrainAndTest.sh
@@ -4,6 +4,8 @@ for i in `ls dataset/train_screenshot/`
 do
 	echo PIG NUM: $i
 	
+	mkdir -p "dataset/train_screenshot_segementation/$i"
+	
 	python3 segmentation_keras/predict.py --weights_path segmentation_keras/pretrained/dilation8_pascal_voc.npy  --input_path "dataset/train_screenshot/$i" --output_path "dataset/train_screenshot_segementation/$i"
 	
 done
